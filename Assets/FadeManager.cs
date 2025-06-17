a90931d49ce030caf3f4711eabaf9617d0feddfc
Karan Ghoghari <karanghoghari303@gmail.com>
2025-06-17 12:45:34 +0200

    Added Fade out transition before scene change on mailbox trigger

---
+++ Assets/FadeManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FadeManager : MonoBehaviour
+{
+    public static FadeManager Instance;
+    public Animator fadeAnimator;
+    public Image fadeImage;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            // Also keep FadePanel alive
+            if (fadeAnimator != null)
+            {
+                DontDestroyOnLoad(fadeAnimator.gameObject);
+            }
+            if (fadeImage != null)
+            {
+                DontDestroyOnLoad(fadeImage.gameObject);
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    public void FadeToScene(string sceneName)
+    {
+        StartCoroutine(FadeAndSwitch(sceneName));
+    }
+
+    private IEnumerator FadeAndSwitch(string sceneName)
+    {
+        fadeAnimator.SetTrigger("StartFadeOut"); // 🔄 only play when triggered
+        yield return new WaitForSeconds(1f); // wait for fade animation to finish
+        SceneManager.LoadScene(sceneName);
+    }
+}
--- Assets/MailboxTeleport.cs
+++ Assets/MailboxTeleport.cs
@@ -1,15 +1,14 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MailboxTeleport : MonoBehaviour
 {
-    public string targetSceneName = "NewspaperArea"; // Change this to your actual scene name
+    public string targetSceneName = "NewspaperArea";
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(targetSceneName);
+            FadeManager.Instance.FadeToScene(targetSceneName);
         }
     }
 }
