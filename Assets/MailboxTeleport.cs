8eebbe97ddba8fcb649f50c70a504d3b8d287887
vn568 <vanessa.paulus@gmx.net>
2025-07-04 20:37:33 +0200

    anisunrise animation
    animation now also plays when you leave scene by walking past the mailbox in the citywall scene

--- Assets/MailboxTeleport.cs
+++ Assets/MailboxTeleport.cs
@@ -1,14 +1,24 @@
+using System.Collections;
 using UnityEngine;
 
 public class MailboxTeleport : MonoBehaviour
 {
     public string targetSceneName = "NewspaperArea";
+    [SerializeField] GameObject sunrise;
+    [SerializeField] int animationDuration;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            StartCoroutine(playAnimation());
             FadeManager.Instance.FadeToScene(targetSceneName);
         }
+    }
+
+    IEnumerator playAnimation()
+    {
+        sunrise.SetActive(true);
+        yield return new WaitForSeconds(animationDuration);
     }
 }
